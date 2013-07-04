5a28b5544b9a93f0dd04bc38242108b53b813562
adnan <mail@adnanwahab.com>
2013-07-04 14:04:15 -0700

    add model

--- models.js
+++ models.js
@@ -1,1 +1,19 @@
-Graph = new Meteor.Collection('graph');
+Graph = new Meteor.Collection('graph', { transform: transform });
+
+var Node =
+    { save: function (update) {
+        Graph.update({ _id: this._id }, _.extend(this, update))
+      }
+    , clone: function () {
+        return transform(_.clone(this))
+      }
+    , destroy: function (){
+        Graph.remove({ _id: this._id })
+      }
+    }
+
+
+function transform(doc) {
+  return _.extend(Object.create(Node),
+           _.omit(doc, 'selected'))
+}
