a8e40ea507483e7fda856e877f4329323eb01e7f
Mark Reveley <mreveley@gmail.com>
2013-10-26 18:22:50 -0700

    abstracted bufferLoader

--- client/audio/sounds.js
+++ client/audio/sounds.js
@@ -1,34 +1,107 @@
-(function() {
-// Fix up prefixing
-var kickBuffer = null;
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
+// (function() {
 
-function loadSound(url) {
+this.sound_init = sound_init;
+
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
   var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
 
-  // Decode asynchronously
+  var loader = this;
+
   request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-    	kickBuffer = buffer;
-    }); 
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        // export the buffer list to window - trying for buffer persistence
+        window.bufferList = loader.bufferList;
+        if (++loader.loadCount == loader.urlList.length) loader.onload(loader.bufferList);
+      },
+      function(error) {
+        // console.error('decodeAudioData error', error);
+      }
+    );
   }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
   request.send();
 }
 
-function playSound(buffer) {
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
+BufferLoader.prototype.load = function() {
+  console.log("in BufferLoader.load")
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
 }
 
-loadSound("audio-files/808ish-Kick-1.wav")
+// window.onload = sound_init;
+window.context;
+var bufferLoader;
+
+function sound_init() {
+  console.log("sound_init called")
+  var contextClass;
+  // from http://chimera.labs.oreilly.com/books/1234000001552/ch01.html#s01_2
+  contextClass = (window.AudioContext || 
+    window.webkitAudioContext || 
+    window.mozAudioContext || 
+    window.oAudioContext || 
+    window.msAudioContext);
+  if (contextClass) {
+    // Web Audio API is available.
+    window.AudioContext = contextClass;
+  } else {
+    console.log("Web Audio API is not available. Ask the user to use a supported browser.");
+  }
+
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      '../sounds/808ish-Kick-1.wav',
+      '../sounds/HH_606_PEQ1.wav',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  console.log("finishedLoading called")
+  // Create two sources and play them both together.
+  window.source1 = context.createBufferSource();
+  window.source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(context.currentTime + 1);
+  source2.start(0);
+}
 
 $('.play').click(function() {
-	playSound(kickBuffer);
+  playSound(kickBuffer);
 })
 
-})();
+// })();
+
